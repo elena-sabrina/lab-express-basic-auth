8c0b7d55c39d42c64d8a99fcb0b12696573567b2
Elena Hadjimina <hadjimina.elena@gmail.com>
2021-01-23 22:08:08 +0800

    first commit

--- app.js
+++ app.js
@@ -1,34 +1,63 @@
-const { join } = require('path');
-const express = require('express');
-const createError = require('http-errors');
-const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware');
-const serveFavicon = require('serve-favicon');
+const path = require("path");
+const express = require("express");
+const createError = require("http-errors");
+const logger = require("morgan");
+const sassMiddleware = require("node-sass-middleware");
+const serveFavicon = require("serve-favicon");
+const connectMongo = require("connect-mongo");
+const MongoStore = connectMongo("expressSession");
+const mongoose = require("mongoose");
 
-const indexRouter = require('./routes/index');
+const authenticationRouter = require("./routers/authentication");
+
+//const User = require("./models/user");
 
 const app = express();
 
 // Setup view engine
-app.set('views', join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(express.static(join(__dirname, 'public')));
-app.use(serveFavicon(join(__dirname, 'public/images', 'favicon.ico')));
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    //Cookie related options
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 15 * 24 * 60 * 60 * 1000
+    },
+    // Database related options
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60
+    })
+  })
+);
 
-app.use(logger('dev'));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static(join(__dirname, "public")));
+app.use(serveFavicon(join(__dirname, "public/images", "favicon.ico")));
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
 app.use(
   sassMiddleware({
-    src: join(__dirname, 'public'),
-    dest: join(__dirname, 'public'),
-    outputStyle: process.env.NODE_ENV === 'development' ? 'nested' : 'compressed',
-    force: process.env.NODE_ENV === 'development',
+    src: join(__dirname, "public"),
+    dest: join(__dirname, "public"),
+    outputStyle:
+      process.env.NODE_ENV === "development" ? "nested" : "compressed",
+    force: process.env.NODE_ENV === "development",
     sourceMap: true
   })
 );
 
-app.use('/', indexRouter);
+
+//app.get (..home)
+//app.get (..profile)
+
+
+//Mount routers
+app.use("/", indexRouter);
 
 // Catch missing routes and forward to error handler
 app.use((req, res, next) => {
@@ -39,10 +68,10 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   // Set error information, with stack only available in development
   res.locals.message = error.message;
-  res.locals.error = req.app.get('env') === 'development' ? error : {};
+  res.locals.error = req.app.get("env") === "development" ? error : {};
 
   res.status(error.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
-const { Router } = require('express');
+const express = require("express");
 const router = Router();
 
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/authentication/sign-in", (req, res, next) => {
+  res.render("sign-in");
+});
+
+router.get("/authentication/sign-out", (req, res, next) => {
+  res.render("sign-out");
+});
+
+router.get("/profile", (req, res, next) => {
+  res.render("profile");
 });
 
 module.exports = router;
